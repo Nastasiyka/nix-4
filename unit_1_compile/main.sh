7be42aea5cd2bd0437a90838eca353c3fef90c5d
Nastasiyka <anastasiia.chuiko@nure.ua>
2021-03-05 11:46:09 +0200

    final commit

--- unit_1_compile/main.sh
+++ unit_1_compile/main.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
-cd terminal_compile
-javac -sourcepath ./src -d build/classes -cp ./libs/commons-lang3-3.11.jar:./libs/commons-collections4-4.3.jar src/ua/com/alevel/check/Check.java  src/ua/com/alevel/count/Count.java  src/ua/com/alevel/Main.java
-java -cp build/classes/:./libs/commons-lang3-3.11.jar:./libs/commons-collections4-4.3.jar:. ua.com.alevel.Main
 
+cd terminal_compile
+. ./terminal.sh
+
+# Рассчитано на то, что переменная ANT_HOME уже есть
 cd ../ant_compile
-ant clean
-ant compile
-ant jar
-ant run
+. ./ant.sh
 
 cd ../maven_compile/maven_app
-mvn clean install
-java -jar target/maven_app-1.0-SNAPSHOT.jar
+. ./maven.sh
---
+++ unit_1_compile/maven_compile/maven_app/maven.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+mvn clean install
+java -jar target/maven_app-1.0-SNAPSHOT.jar
